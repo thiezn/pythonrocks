b301be994ed15aaed32d3fb06adcb26faa4cd646
Mortimer, M (Mathijs) <mathijs.mortimer@rabobank.com>
2018-05-03 14:42:39 +0200

    Prettify

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ def hello_world():
     html = (
         "<h1>Hello world!!<h1>"
         "<h2>Summary</h2>"
+        "<font size="6">"
         "<b>Python</b> shooting for the stars!<br>"
         "<i>C#</i> The only way is down<br>"
         "<i>PowerShell</i> Staat niet op de kaart... bieb bieb bieb mwueup<br>"
+        "</font><br><br>"
         "<img src='https://raw.githubusercontent.com/thiezn/pythonrocks/master/images/programmingpopularity.PNG'>"
         "<p>nice going, you managed to deploy some code the cool way"
     )
