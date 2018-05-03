b89518b824aef9a34d0b35fa2518ca410f47ad7d
Mortimer, M (Mathijs) <mathijs.mortimer@rabobank.com>
2018-05-03 14:29:42 +0200

    FIX missing return value

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def hello_world():
         "<h2>Hello world!!<h2>"
         "<p>nice going, you managed to deploy some code the cool way"
     )
+    return html
 
 if __name__ == '__main__':
     app.run()
